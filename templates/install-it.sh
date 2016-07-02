3f3f43c9d2a8d206ed590d0e685b2a4ecfcefff1
Peter DeMartini <thepeterdemartini@gmail.com>
2016-07-02 12:14:25 -0700

    fixes

--- templates/install-it.sh
+++ templates/install-it.sh
@@ -1,4 +1,4 @@
-curl -s "https://raw.githubusercontent.com/peterdemartini/butterfinger/master/scripts/init.sh" | env \
+curl -s "https://raw.githubusercontent.com/peterdemartini/butterfinger/master/scripts/init.sh?r=$(random)" | env \
   PLEX_USERNAME='[username]' \
   PLEX_PASSWORD='[password]' \
   bash
